e9641de737b6dd5f66a7f6f8907f4f31f694d8f2
azouever <azouever@gmail.com>
2022-04-01 18:52:25 +0800

    hmr

--- webpack-library/webpack.config.js
+++ webpack-library/webpack.config.js
@@ -1,44 +1,21 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-    //     index: {
-    //       import: "./src/index.js",
-    //       dependOn: "shared",
-    //     },
-    //     another: {
-    //       import: "./src/another-module.js",
-    //       dependOn: "shared",
-    //     },
-    //     shared: "lodash",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Caching",
-    }),
-  ],
+  entry: "./src/index.js",
   output: {
-    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
+    filename: "webpack-numbers.js",
+    library: {
+      name: "webpackNumbers",
+      type: "umd",
+    },
   },
-  experiments: {
-    topLevelAwait: true,
-  },
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
+  externals: {
+    lodash: {
+      commonjs: "lodash",
+      commonjs2: "lodash",
+      amd: "lodash",
+      root: "_",
     },
   },
 };
